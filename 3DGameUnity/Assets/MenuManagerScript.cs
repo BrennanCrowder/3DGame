24a20adbd0c2a41fc698d2fab0176b83b05c04d3
Brennan Crowder <56270795+BrennanCrowder@users.noreply.github.com>
2021-12-06 17:31:29 -0600

    Lots of changes

---
+++ 3DGameUnity/Assets/MenuManagerScript.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagerScript : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    public GameObject settingCanvas;
+    public GameObject musicObject;
+    public GameObject RoomsParent;
+    public GameObject[] Rooms;
+    public GameObject onlyRoom;
+    private bool inMenu = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ChangeRoom");
+    }
+
+    public void ButtonStart()
+    {
+        inMenu = false;
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonSettings()
+    {
+        settingCanvas.SetActive(true);
+        menuCanvas.SetActive(false);
+    }
+
+    public void ButtonQuit()
+    {
+        Debug.Log("Quitting Application...");
+        Application.Quit();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        // TODO
+    }
+
+    public void ButtonBack()
+    {
+        menuCanvas.SetActive(true);
+        settingCanvas.SetActive(false);
+    }
+
+    public void ButtonToggleTimer()
+    {
+        // TODO: Make Timer
+    }
+
+    public void ButtonToggleDevMiniMap()
+    {
+        // TODO: Make Static Var
+    }
+
+    IEnumerator ChangeRoom()
+    {
+        while (inMenu) {
+            yield return new WaitForSeconds(2);
+            int randInt = Random.Range(0, Rooms.Length);
+            SwapRooms(Rooms[randInt]);
+        }
+        yield return 1;
+    }
+
+    void SwapRooms(GameObject newRoom)
+    {
+        onlyRoom.transform.GetChild(0).gameObject.SetActive(false);
+        onlyRoom.transform.GetChild(0).transform.SetParent(RoomsParent.transform);
+        newRoom.transform.SetParent(onlyRoom.transform);
+        newRoom.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
